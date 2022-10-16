36e0fbd083de82623134c80298117b9f92059bb8
crafthedgehog <114859225+crafthedgehog@users.noreply.github.com>
2022-10-16 16:37:50 +0300

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int n = 10;
+    int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t"; 
+      
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t"; 
+    }
+    cout << endl;
+}
